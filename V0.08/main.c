a32636e3e539d6fb546d45717d91604fd14a7134
LemonFountainPerson <usernameguy2446@gmail.com>
2026-01-13 23:37:20 +0000

    File renaming

--- V0.08/main.c
+++ V0.08/main.c
@@ -11,9 +11,7 @@ int main(void)
 
 // 64 Bit SDL
 /*
- gcc main.c LemonMain.c gameObjects.c levelLoader.c drawScreen.c playerController.c spriteLoader.c soundProcessor.c eventManager.c animations.c UIObjects.c cutsceneManager.c enemies.c 
--o LemonEngine_SDL_64Bit.exe -O3 
--I C:\Libraries\gcc_64\include -L C:\Libraries\gcc_64\lib -lSDL3 -lSDL3_ttf -lSDL3_image -m64 -Wall -Werror
+gcc main.c LemonMain.c gameObjects.c levelLoader.c drawScreen.c playerController.c spriteLoader.c soundProcessor.c eventManager.c animations.c UIObjects.c cutsceneManager.c enemies.c -o LemonEngine.exe -O3 -I C:\Libraries\gcc_64\include -L C:\Libraries\gcc_64\lib -lSDL3 -lSDL3_ttf -lSDL3_image -m64 -Wall -Werror
 
 
 Control structure:
